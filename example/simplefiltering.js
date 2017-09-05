4219a6fa94558dab0e2ce4e4c2a53781bc071168
Philip Crotwell <crotwell@seis.sc.edu>
2017-09-05 16:15:43 -0400

    remove table of records

--- example/simplefiltering.js
+++ example/simplefiltering.js
@@ -28,64 +28,6 @@ divP.append("a")
     .text(url);
 
 function processMiniseed(records) {
-var table = d3.select("div.miniseed")
-        .select("table");
-      if ( table.empty()) {
-        table = d3.select("div.miniseed")
-          .append("table");
-        var th = table.append("thead").append("tr");
-        th.append("th").text("Seq");
-        th.append("th").text("Net");
-        th.append("th").text("Sta");
-        th.append("th").text("Loc");
-        th.append("th").text("Chan");
-        th.append("th").text("Start");
-        th.append("th").text("End");
-        th.append("th").text("NumSamp");
-        th.append("th").text("Sps");
-        table.append("tbody");
-      }
-      var tableData = table.select("tbody")
-        .selectAll("tr")
-        .data(records, function(d) { return d.codes()+d.header.start;});
-      tableData.exit().remove();
-      var tr = tableData.enter().append('tr');
-      tr.append("td")
-        .text(function(d) {
-          return d.header.seq;
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.netCode;
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.staCode;
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.locCode;
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.chanCode;
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.start.toISOString();
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.end.toISOString();
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.numSamples;
-        });
-      tr.append("td")
-        .text(function(d) {
-          return d.header.sampleRate;
-        });
 
       let seismogram = miniseed.merge(records);
 for(let i=0; i<seismogram.length; i++) {
